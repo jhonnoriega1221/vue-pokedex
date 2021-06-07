7610abc4e001a2533ab7fe1dd666bb306a526715
jhonnoriega1221 <jhonlex17@gmail.com>
2021-06-07 16:09:57 -0500

    Lista de pokemons conectada con la api

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="pokecard">
+    <div class="pokecard" v-if="pokemonData">
         <v-card
         class="pa-3"
         style="border-radius: 20px"
@@ -7,29 +7,27 @@
         flat
         ripple
         dark
-        to="pokemon/1"
+        :to="{ name: 'PokemonInfo', params: { id: pokemonData.id} }"
         >
+        
             <!--Pokeball background
             <img class="pokeball-bg" src="../assets/pokeball.svg" alt="">
             -->
 
             <!--Pokemon number-->
-            <v-card-text style="height:20px" class="pa-0 text-h6 text-right font-weight-black text--disabled black--text">#001</v-card-text>
+            <v-card-text style="height:20px" class="pa-0 text-h6 text-right font-weight-black text--disabled black--text">#{{pokemonData.id}}</v-card-text>
              
 
             <!--Pokemon name-->
-            <v-card-title class="text-body pt-0 pl-0 font-weight-bold">Bulbasaur</v-card-title>
+            <v-card-title class="text-capitalize text-body pt-0 pl-0 font-weight-bold d-block text-truncate">{{pokemonData.name}}</v-card-title>
 
 
             <v-row>
                 <!--Pokemon Types-->
                 <v-col class="pb-0 pr-0 pt-0" style="//background:green" cols="6">
                     
-                    <v-chip style="height:25px" class="mb-1 d-block text-center" color="rgba(255, 255, 255, 0.2)">
-                        <span class=" text-caption">Planta</span>
-                    </v-chip>
-                    <v-chip style="height:25px"  class="mb-1 d-block text-center" color="rgba(255, 255, 255, 0.2)">
-                        <span class="text-caption">Veneno</span>
+                    <v-chip v-for="(type) in pokemonData.types" v-bind:key="type.id" style="height:25px" class="text-capitalize mb-1 d-block text-center text-caption" color="rgba(255, 255, 255, 0.2)">
+                        {{type.type.name}}
                     </v-chip>
 
                 </v-col>
@@ -37,7 +35,7 @@
                 <!--Pokemon artwork-->
                 <v-col class="pa-0" style="//background:blue" cols="6">
                     
-                    <v-img  src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></v-img>
+                    <v-img  v-bind:src='pokemonData.sprites.other["official-artwork"].front_default'></v-img>
 
                 </v-col>
             </v-row>
@@ -47,8 +45,31 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Pokecard'
+    name: 'Pokecard',
+    props:{
+        pokemonURL: Object
+    },
+    data: () => {
+        return{
+            pokemonData: '',
+            testPath: 'pokemon/2'
+        }
+    },
+    mounted(){
+        this.getPokemonData();
+    },
+    methods: {
+        getPokemonData(){
+            axios.get(this.pokemonURL.url).then(
+                res => {
+                    this.pokemonData = res.data
+                }
+            )
+        }
+    }
 }
 </script>
 
--- src/views/Pokedex.vue
+++ src/views/Pokedex.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="pokedex">
+    <div class="pokedex" v-if="pokemonsURLList">
         <h1 class="pb-7">Pokedex</h1>
         <v-row>
             <v-col 
             class="pa-2" cols="6" sm="4" md="3" lg="3" xl="2" v-for="(pokemon, i) in 12"
             v-bind:key="i">
-                <Pokecard/>
+                <Pokecard v-bind:pokemonURL="pokemonsURLList[i]"/>
             </v-col>
         </v-row>
         
@@ -14,9 +14,29 @@
 
 <script>
 import Pokecard from '@/components/PokemonCard.vue'
+
+import axios from 'axios';
+
     export default {
         components: {
             Pokecard
+        },
+        data: () => {
+            return {
+                pokemonsURLList: ''
+            }
+        },
+        mounted(){
+            this.getPokemonsURLs()
+        },
+        methods:{
+            async getPokemonsURLs(){
+                await axios.get('https://pokeapi.co/api/v2/pokemon?limit=12&offset=0').then(
+                    res => {
+                        this.pokemonsURLList = res.data.results
+                    }
+                )
+            }
         }
     }
 </script>
