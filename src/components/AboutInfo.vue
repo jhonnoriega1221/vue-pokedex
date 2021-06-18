1a52033640c277379d9277e73b7fcd3c76bc5250
jhonnoriega1221 <jhonlex17@gmail.com>
2021-06-18 10:27:55 -0500

    Agregada conexión con firebase y botón de cerrar dialogo en acerca de la pokedex

--- src/components/AboutInfo.vue
+++ src/components/AboutInfo.vue
@@ -6,15 +6,15 @@
 	>
 		<v-card>
 			<v-card-title class="">
-				Pokedex
+				Acerca de Pokédex
 			</v-card-title>
 			<div class="d-flex">
-				<v-img transition=none src="../assets/logo.png" width="30" class="ma-8"></v-img>
-				<v-img transition=none src="../assets/logo2.png" width="70" class="ma-1"></v-img>
+				<v-img transition=none src="../assets/logo.png" width="10" class="ma-8"></v-img>
+				<v-img transition=none src="../assets/logo2.png" width="60" class="ma-1"></v-img>
 			</div>
 			<v-card-text class="text-center">Creado con <a href="https://vuejs.org/" target="_blank">Vue</a> y <a href="https://vuetifyjs.com/en/" target="_blank">Vuetify</a></v-card-text>
 
-			<v-img transition=none src="../assets/person.png" width="100" class="ma-auto"></v-img>
+			<v-img transition=none src="../assets/person.png" width="50" class="ma-auto"></v-img>
 
 			<v-card-text class="text-center">
 				Creado por
@@ -23,8 +23,21 @@
 				<br>
 			</v-card-text>
 			<v-card-text class="text-right">
-				v0.1
+				v0.1 | 2021
 			</v-card-text>
+
+			<v-divider></v-divider>
+
+			<v-card-actions>
+				<v-spacer></v-spacer>
+				<v-btn
+					color="primary"
+					text
+					v-on:click="closeDialog"
+				>
+					Cerrar
+				</v-btn>
+			</v-card-actions>
 		</v-card>
 	</v-dialog>
 </template>
